e38f27ca181853975200c44da536ee4ffdc32ce1
Samet Yatmaz <>
2020-05-10 13:56:39 +0300

    Intro to protocol associated type was developed

--- Intro Protocol Associated Type.playground/Contents.swift
+++ Intro Protocol Associated Type.playground/Contents.swift
@@ -1,3 +1,55 @@
 import UIKit
 
-var str = "Hello, playground"
+/*
+    Intro to Associated Type
+    Problem: How to create generic protocols
+*/
+
+/*
+    Note: Type must be defined before compile
+ */
+
+//: Design Generic Struct (Review)
+struct GenericStruct<T> {
+    var property: T?
+}
+
+let explicitly = GenericStruct<Bool>()
+// T is Bool
+let implicitly = GenericStruct(property: "Samet")
+// T is String
+
+//: Design Normal Protocol
+protocol NormalProtocol {
+    var property: String { get set }
+}
+
+//: Design Class
+class NormalClass: NormalProtocol {
+    var property: String = "Samet"
+}
+
+
+/*
+    Introducing Generic Protocol
+*/
+
+protocol GenericProtocol {
+    associatedtype myType
+    var anyProperty: myType { get set }
+}
+
+//: Define Associated Type
+struct SomeStruct: GenericProtocol {
+    var anyProperty = 1996  // myType = Int
+}
+
+struct NewStruct: GenericProtocol {
+    var anyProperty = "Samet" // myType = String
+}
+
+//: Define Associated Type with Typealias
+struct ExplicitStruct: GenericProtocol {
+    typealias myType = Bool
+    var anyProperty = true
+}
